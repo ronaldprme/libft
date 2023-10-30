27a7d594355cd545857043e3023e68f14f523035
Ronald <ronaldpr@me.com>
2023-10-30 07:52:56 +0000

    teste

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,15 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rprocopi <rprocopi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rprocopi <rprocopi@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 13:01:13 by rpr               #+#    #+#             */
-/*   Updated: 2023/10/28 10:34:34 by rprocopi         ###   ########.fr       */
+/*   Updated: 2023/10/30 07:52:12 by rprocopi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+ 
 //Transforma string para int
 int	ft_atoi(const char *nptr)
 {
